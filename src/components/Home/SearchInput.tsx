f6b2386adfc987841df904e9d702b014a35ed7be
tobby8629 <sarhmullah15@gmail.com>
2025-01-31 00:56:20 -0600

    Completed Footer

--- src/components/Home/SearchInput.tsx
+++ src/components/Home/SearchInput.tsx
@@ -1,18 +1,19 @@
 interface search {
   className?: string
+  inputStyle?: string
 }
 
 import { arrow } from "../../../public/svgs"
 
 
-const SearchInput = ({className}: search) => {
+const SearchInput = ({className, inputStyle}: search) => {
   return (
     <div className={`flex justify-between mt-4 items-center pl-5 border-2 border-gray-400 bg-white h-14 max-sm:h-11 w-[401px] max-sm:w-full max-lg:mx-auto rounded-[2rem] ${className}`}>
       <input 
       placeholder="Email"
       className="searchInput"
       />
-      <span className="bg-pry h-full translate-x-1 flex justify-center items-center rounded-full max-sm:w-11 max-sm:h-11 w-14">
+      <span className={`bg-pry h-full translate-x-1 flex justify-center items-center rounded-full max-sm:w-11 max-sm:h-11 w-14 ${inputStyle}`}>
         <img src={arrow} />
       </span>
     </div>
--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -5,28 +5,28 @@ import SearchInput from '../Home/SearchInput'
 const Footer = () => {
   return (
     <div className='bg-[#EFD1F3] py-10'>
-      <section className='max-sm:!m-0' >
-        <div className='max-sm:w-[85%]'>
+      <section className='max-sm:!m-0 md:flex justify-between lg:w-[85%]' >
+        <div className='max-sm:w-[85%] w-[268px]'>
           <img src={logo} alt='logo' />
-          <p className='text max-sm:!text-left max-sm:text-sm my-3'>
+          <p className='text !text-left max-sm:text-sm !text-base my-3'>
             why we are different, Sign Up now to get early access
           </p>
-          <div className='flex items-center gap-3'>
+          <div className='flex items-center gap-3 md:mt-5'>
             {socials.map((e)=>(
               <img src={e} alt='img' key={e}/>
             ))}
           </div>
         </div>
-        <div className='my-7 font-mulish capitalize'>
+        <div className='max-sm:mt-20 font-mulish capitalize lg:w-[calc(80%-268px)] xl:w-[calc(70%-268px)]'>
           <h5 className='text-lg font-semibold text-left text-gray-700'>subscribe to Our Newsletter</h5>
-          <SearchInput />
-          <div className='flex my-3 justify-between gap-5'>
+          <SearchInput className='h-12 md:w-[300px]  max-sm:!w-[85%] !mx-0 lg:w-[350px]' inputStyle='w-12'  />
+          <div className='flex my-5 justify-between max-sm:gap-5 gap-5'>
             {otherLink.map((e)=>(
               <div key={e.header} className='text-left' >
                 <h3 className='mb-2'>{e.header}</h3>
                 <ul>
                   {e.links.map((each)=>(
-                    <li className='text max-sm:!text-xs mb-2 text-left' key={each}>{each}</li>
+                    <li className='text max-sm:!text-xs mb-2 !text-sm text-left' key={each}>{each}</li>
                   ))}
                 </ul>
               </div>
